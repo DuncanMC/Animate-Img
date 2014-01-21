4c48210ac8ddcf96a8972f1d45ad5825b20c9a97
Duncan Champney <dchampney@wareto.com>
2014-01-20 20:42:26 -0500

    Initial Commit

---
+++ Animate Img/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Animate Img
+//
+//  Created by Duncan Champney on 1/20/14.
+//  Copyright (c) 2014 WareTo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Animate Img/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Animate Img
+//
+//  Created by Duncan Champney on 1/20/14.
+//  Copyright (c) 2014 WareTo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
